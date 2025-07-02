001f4b2616cff71b8a8235efa92c2e2788d27be3
IN923 <inderjeetsingh0521@gmail.com>
2025-07-02 07:06:39 +0530

    second commit

--- Task_Manager/task/views.py
+++ Task_Manager/task/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views.generic.edit import CreateView
 from django.views import View
 from .models import Task
 from django.http import JsonResponse
@@ -6,6 +7,12 @@ from django.http import JsonResponse
 
 class All_Task(View):
     def get(self,request,*args,**kwargs):
-        tasks = Task.objects.all()
-        all_tasks = JsonResponse(tasks,safe=False)
-        return all_tasks
+        tasks = Task.objects.all().values()
+        all_tasks = list(tasks)
+        return JsonResponse(all_tasks,safe=False)
+    
+class Create_task(CreateView):
+    model = Task
+    fields = ['name']
+    template_name = 'task/task_create_form.html'
+    success_url = 'create'
