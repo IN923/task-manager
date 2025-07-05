5e936a42c8103b0b66c353a37674e0158b76eaa0
IN923 <inderjeetsingh0521@gmail.com>
2025-07-05 07:41:27 +0530

    Implemented create task functionality

--- Task_Manager/task/urls.py
+++ Task_Manager/task/urls.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 from django.urls import path
-from .views import All_Task,Create_task
+from .views import IndexView,CreateTask
 
 urlpatterns = [
-    path('all_task',All_Task.as_view()),
-    path('create',Create_task.as_view()),
+    path('',IndexView.as_view()),
+    path('add/',CreateTask.as_view()),
 ]
 
 # https://identity.getpostman.com/client-auth/confirm?auth_challenge=0141232f3f2bd77158f7215babb9eb998aaaa6fdb5bcde67dc6d60fde296bef1&auth_device=app_native&auth_device_version=11.39.2
--- Task_Manager/task/views.py
+++ Task_Manager/task/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 from django.views.generic.edit import CreateView
 from django.views import View
 from .models import Task
 from django.http import JsonResponse
+import json
 # Create your views here.
+
+class IndexView(View):
+    def get(self,request):
+        return render(request,'task/index.html')
 
 class All_Task(View):
     def get(self,request,*args,**kwargs):
@@ -11,8 +17,16 @@ class All_Task(View):
         all_tasks = list(tasks)
         return JsonResponse(all_tasks,safe=False)
     
-class Create_task(CreateView):
-    model = Task
-    fields = ['name']
-    template_name = 'task/task_create_form.html'
-    success_url = 'create'
+class CreateTask(CreateView):
+
+    def post(self,request,*args,**kwargs):
+        # self.object = None
+        # return super().post(request,*args,**kwargs)
+        body_unicode = request.body.decode('utf-8')
+        task_name = json.loads(body_unicode)
+        if len(task_name)==0:
+            return JsonResponse({'error':'Task is Empty'},status=400)
+        return JsonResponse(f'{task_name}',safe=False)
+    
+class UpdateTask():
+    pass
